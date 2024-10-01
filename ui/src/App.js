3a6e80e1ea49c8f1c5ba2afd4b4bae44544d61c2
DangTruongPhat2004 <dangtruongphat71@gmail.com>
2024-10-01 08:35:55 +0700

    modified: ui/src/App.js

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import Layout from './layouts/Layout'; 
+import Profile from './Pages/Profile/Profile'
+import Salon from './Pages/Salon/Salon'; 
+import Staff from './Pages/Staff/Staff';
+import Wage from './Pages/Wage/Wage';
+import Booking from './Pages/Booking/Booking';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Layout>
+      <Router>
+        <Routes>
+            {/* Các route con sẽ được render trong Outlet của Layout */}
+            {/* <Route path="dashboard" element={<Dashboard />} /> */}
+            <Route path="/" element={<Navigate to="/profile" replace />} />
+            <Route path="profile" element={<Profile />} />
+            <Route path="salon" element={<Salon />} />
+            <Route path="staff" element={<Staff />} />
+            <Route path="wage" element={<Wage />} />
+            <Route path="booking" element={<Booking />} />
+        </Routes>
+      </Router>
+    </Layout>
+  )
 }
 
-export default App;
+export default App
